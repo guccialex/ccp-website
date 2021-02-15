61e7b257239b16a26d69cde4ceaba97562d98d0c
alexander <alex.gusain@gmail.com>
2021-02-15 16:17:21 -0500

    -

--- static/gamefinder_static/script.js
+++ static/gamefinder_static/script.js
@@ -3,8 +3,10 @@
 //http://0.0.0.0:8082/index.html?port=2432&password=F38d
 
 
-console.log("updated with scriptjs working");
 
+
+
+console.log("updated with scriptjs working");
 
 let baseurl = window.location.origin;
 
@@ -18,6 +20,7 @@ console.log(gamefilesurl);
 console.log(baseurl);
 
 
+let foundgame = "";
 
 
 let xmlHttp = new XMLHttpRequest();
@@ -27,6 +30,9 @@ let xmlHttp = new XMLHttpRequest();
 xmlHttp.onreadystatechange = function() {
 
     console.log(xmlHttp.responseText);
+
+    document.getElementById("demo").innerHTML = "connection failed :(";
+
 
     let response = JSON.parse( xmlHttp.responseText );
 
@@ -46,25 +52,21 @@ xmlHttp.onreadystatechange = function() {
     //change the "connect to public game" button to green
     //and then when you click it again it sends you to the game
     
-
-    //window.open(fulladdress); 
-    var x = document.getElementById("myDIV");
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
+    foundgame = fulladdress;
 
 
 
+    document.getElementById("demo").innerHTML = "";
 
+    var x = document.getElementById("GameFound");
+    x.style.display = "block";
 
 };  
 
 
 function ConnectToFoundGame() {
 
-
+    window.open(foundgame);
 
 }
 
@@ -80,6 +82,17 @@ function ConnectToPublicGame() {
     xmlHttp.send();
     
 }
+
+
+
+/*
+
+    <button onclick="CreatePrivateGame()">create a private game</button>
+    <br>
+    <br>
+    <input type="text" id="gamepassword" value="password">
+    <button onclick="ConnectToPrivateGame()">join a private game</button>
+    <br>
 
 
 function ConnectToPrivateGame() {
@@ -100,4 +113,4 @@ function CreatePrivateGame() {
 }
 
 
-
+*/
